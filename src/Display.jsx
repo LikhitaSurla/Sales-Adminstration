b9da3b3fdbe1141c5efc027ecdd1b00645bb1002
LikhitaSurla <slikhita04@gmail.com>
2024-01-01 11:23:06 +0530

    secondd

--- src/Display.jsx
+++ src/Display.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Display() {
   return (
     <>
-    <div>hii</div></>
+    <div>Who are using this ?</div>
+    <button>Admin</button>
+    <button>User</button>
+    </>
   )
 }
