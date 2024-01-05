112f129349c9471b86c0f3b571d639c6617a4315
LikhitaSurla <slikhita04@gmail.com>
2024-01-05 20:04:16 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,25 @@ function App() {
   const [userName,setUserName]=useState('');
   const [password,setPassword]=useState('');
   const [state,setState] = useState(false);
-  
   const[dataArray,setDataArray] = useState([{}]);
+  const [matchFound,setMatchFound]=useState(false);
 
   useEffect(()=>{
     const dataFetching = async()=>{
+
+      
       try{
        const temp = await fetchData();
+       setDataArray(temp);
+       temp.forEach((doc)=>{
+        const data= doc.data();
+        console.log(data)
+        if(data.name==userName && data.password==password){
+          setMatchFound(true);
+    
+         }
+       })
+       
        
       }catch(err){
         console.log(err);
@@ -25,13 +37,24 @@ function App() {
     dataFetching();
   },[]);
 
-  if(state==false){
+  const submitBtn =()=>{
+    console.log(matchFound)
+    if(matchFound==true){
+      setState(true);
+     }
+  }
+
+  if( state==false){
   return (
     <>
     <input type="text" placeholder='enter your name' onChange={(e)=>setUserName(e.target.value)} />
     <input type="password" placeholder='enter your password' onChange={(e)=>setPassword(e.target.value)} />
-    {/* <button onClick={submitBtn}> submit</button> */}
+    <button onClick={submitBtn}> submit</button>
+    
+  
+   
     </>
+    
   )
   }
   else{
@@ -41,5 +64,7 @@ function App() {
       </>
     )
   }
+ 
+
 }
 export default App
