f84b7644ba2156aca3f5e1c7a55750364f0ba6dc
LikhitaSurla <slikhita04@gmail.com>
2024-01-05 22:25:11 +0530

    admins

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
+import fetchData from './Data';
 
 export default function Admin(props) {
-    const backclick=()=>{
-        props.setAdmin(false)
-        props.setDisplay(true)
+  const [state,setState] =useState(false);
+  const [ownerName,setOwnerName]=useState('');
+  const [ownerPassword,setOwnerPassword]=useState('');
+
+   
+    const ownerClicked =async()=>{
+      let matchfound = false;
+      try{
+          const usersData = await fetchData();
+          usersData.forEach((doc)=>{
+           
+            if(doc.name == ownerName && doc.password == ownerPassword){
+              matchfound = true;
+            
+            }
+          });
+          if(matchfound){
+            setState(true);
+          }
+      }
+      catch(err){
+        console.error(err);
+      }
+  }
+    if(state==false){
+      return (
+        <>
+        
+        <input type="text" placeholder='name' onChange={(e)=>setOwnerName(e.target.value)} />
+        <input type="password" placeholder='password' onChange={(e)=>setOwnerPassword(e.target.value)} />
+        <button
+        >Update my Password</button>
+        <button onClick={ownerClicked}>submit</button>
+
+        </>
+      )
     }
-  return (
-    <>
-    <div>Admin</div>
-    <button onClick={backclick}>Back</button>
-    </>
-  )
+    else{
+      return(
+        <>
+        <div>Sales Deatils</div>
+        <div>Employee Details</div>
+        <div>New Customer count</div>
+        <div>Customer Reviews</div>
+        
+        </>
+      )
+    }
+
 }
