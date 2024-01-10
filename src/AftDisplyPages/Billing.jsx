dfade21cfd80de29aa94b05986ce27530ab33199
KUCHARLAPATI VIJAYA KUMAR VARMA <vijayakumarkucharlapati@gmail.com>
2024-01-10 17:12:00 +0530

    Not done yet

--- src/AftDisplyPages/Billing.jsx
+++ src/AftDisplyPages/Billing.jsx
@@ -1,101 +1,113 @@
-import React,{useEffect, useState} from 'react';
-import fetchProduct from '../FetchingData/ProductDetails';
-import { collection,query,where,getDocs,doc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebase';
 
 export default function Billing() {
-  const productCollectionRef=collection(db,'productdetails')
-  const [obj,setObj]=useState({});
-  const [code,setCode]=useState('');
-  const [quantity,setQuantity]=useState(1);
+  const productCollectionRef = collection(db, 'productdetails');
+  const [items, setItems] = useState([]);
+  const [code, setCode] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  const [prevQuantity, setPrevQuantity] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
 
-  const IncrementQuantity=()=>{
-   setQuantity(quantity+1)
-  }
-  const DecrementQuantity=()=>{
-    setQuantity(quantity-1)
+  // There are small small errors the error is the items are diplaying but when we try to increse
+  //the quantityt of second item the first items quantity and value is also changing check it out
+  const [obj, setObj] = useState({
+    productname: 'None',
+    price: 0,
+  });
 
-  }
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const productData = await fetchProduct();
-          // console.log('Fetched Product Data:', productData);
+  const IncrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
 
-        } catch (error) {
-          console.error(error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-  
-     const searchCode=async()=>{
-        try {
-          
-          const q = query(productCollectionRef, where('code', '==', code));
-          const product = await getDocs(q);
-          const data = [];
-          product.forEach((doc) => {
-            data.push(doc.data());
-          });
-          setObj({
-         productname:data[0].name,
-         price:data[0].price
+  const DecrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
-          })
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      useEffect(() => {
-        console.log(obj);
-      }, [obj]); 
-      
-     const newInput=()=>{
-      `
-      <input type="text" placeholder='code' onChange={(e)=>setCode(e.target.value)}/>
-        <button onClick={searchCode}>Submit</button>
-        <p>Product name:{obj.productname}</p>
-        <p>Price:{obj.price}</p>
-        <p>FinalPrice: {quantity*obj.price}</p>
-          <div>
-        <button onClick={DecrementQuantity}>-</button>
-         <input type="number" placeholder='quantity' value={quantity}/>        
-         <button onClick={IncrementQuantity}>+</button>
-          </div>
-          <button onClick={newInput} id='new'>New</button>`
+  const newInput = () => {
+    setCode('');
+    setQuantity(1);
+    setObj({
+      productname: 'None',
+      price: 0,
+    });
+  };
 
-     }
+  const doneBtn = () => {
+    setFinalPrice(quantity*obj.price)
+    setPrevQuantity(quantity);
+    setItems((prevItems) => [...prevItems, obj]);
+    newInput();
+  };
+
+  const searchCode = async () => {
+    try {
+      const q = query(productCollectionRef, where('code', '==', code));
+      const product = await getDocs(q);
+      const data = [];
+      product.forEach((doc) => {
+        data.push(doc.data());
+      });
+      setObj({
+        productname: data[0].name,
+        price: data[0].price,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+  }, [obj]);
 
   return (
     <div>
       <section>
-        <p>Customer Details</p>
-        <input type="text" placeholder='Customer name'/>
-        <input type="text" placeholder='Customer Number'/>
-
+        <p>Customer details</p>
+        <input type="text" placeholder="Customer name" />
+        <input type="text" placeholder="Customer Number" />
       </section>
-
       <section>
         <p>Items:</p>
-        <input type="text" placeholder='code' onChange={(e)=>setCode(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="code"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
         <button onClick={searchCode}>Submit</button>
-        <p>Product name:{obj.productname}</p>
-        <p>Price:{obj.price}</p>
-        <p>FinalPrice: {quantity*obj.price}</p>
-          <div>
-        <button onClick={DecrementQuantity}>-</button>
-         <input type="number" placeholder='quantity' value={quantity}/>        
-         <button onClick={IncrementQuantity}>+</button>
-          </div>
-          <button onClick={newInput} id='new'>New</button>
-          
-
-          
-          </section>
-        <button>Generate Bill</button>
-      
+        <p>Product name: {obj.productname}</p>
+        <p>Price: {obj.price}</p>
+        <p>Final Price: {quantity * obj.price}</p>
+        <div>
+          <button onClick={DecrementQuantity}>-</button>
+          <input
+            type="number"
+            placeholder="quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+          <button onClick={IncrementQuantity}>+</button>
+        </div>
+        <br />
+        <br />
+      </section>
+      <div>
+        <button onClick={doneBtn}>
+          Done
+        </button>
+      </div>
+      <br />
+      <section>
+        <p>Selected Items:</p>
+          {items.map((item, index) => (
+            <li >
+              {item.productname} - {finalPrice} - Quantity: {prevQuantity}
+            </li>
+          ))}
+      </section>
     </div>
-  )
+  );
 }
