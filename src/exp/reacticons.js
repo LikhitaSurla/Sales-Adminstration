1d8ce93ef0f754414b8ea27deeaf20ba5329aba0
LikhitaSurla <slikhita04@gmail.com>
2024-02-13 20:46:56 +0530

    sepatinig react icons

---
+++ src/exp/reacticons.js
@@ -0,0 +1,14 @@
+import { MdKeyboardBackspace } from "react-icons/md";
+import { FaIdCardClip } from "react-icons/fa6";
+import { FaUserAlt } from "react-icons/fa";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { TbCoinRupeeFilled } from "react-icons/tb";
+import { RiUserHeartFill } from "react-icons/ri";
+import { IoMaleFemale,IoClose } from "react-icons/io5";
+import { FaUserFriends } from "react-icons/fa";
+import { MdOutlinePhoneInTalk } from "react-icons/md";
+import { MdOutlineReceiptLong } from "react-icons/md";
+
+
+export {MdKeyboardBackspace,FaUserFriends,MdOutlinePhoneInTalk,MdOutlineReceiptLong,FaIdCardClip,FaUserAlt,FaPhoneVolume,TbCoinRupeeFilled,RiUserHeartFill,IoMaleFemale,IoClose};
+
